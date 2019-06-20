921372fad2fb0c53491cdbf8dc3f68f7e9b1f92d
Ofek Atar <assassin3673@gmail.com>
2019-06-20 16:09:57 +0300

    Fixed Joystick Activity
    Checkstyle

--- app/src/main/java/com/ISSAatOfek/EX4/JoyStickActivity.java
+++ app/src/main/java/com/ISSAatOfek/EX4/JoyStickActivity.java
@@ -5,23 +5,34 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// The Joystick Screen Activity
 public class JoyStickActivity extends AppCompatActivity implements JoyStickView.JoystickListener{
+    // The Tcp Client for communication.
     private TcpClient tcpClient;
 
+    // The function that runs on creation.
     protected void onCreate(Bundle savedInstanceState) {
+        // Connect to the server.
         new connectToServer().execute();
+        // Init AppCompactActivity
         super.onCreate(savedInstanceState);
+        // Setting the content view to the joystick's layout.
         setContentView(R.layout.activity_joystick);
     }
+    // The function called when the joystick moved.
     public void onJoystickMoved(float xPercent, float yPercent, int source) {
+        // Send joystick's values to the server.
         tcpClient.sendMessage(Float.toString(xPercent),Float.toString(yPercent));
 
     }
+    // The function called upon destruction of the activity.
     @Override
     protected void onDestroy(){
         super.onDestroy();
+        // Stop the connection to the server.
         tcpClient.Stop();;
     }
+    // AsyncTask for connecting to the server
     public class connectToServer extends AsyncTask<String,String,TcpClient>
     {
         @Override
